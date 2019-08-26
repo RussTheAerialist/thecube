b32ad0165ae02b4ab97a34fea78f08d424ce9230
Russell Hay <me@russellhay.com>
2019-08-26 08:22:39 -0700

    checkpoint

--- sensor_node/main/i2c.h
+++ sensor_node/main/i2c.h
@@ -1,3 +1,7 @@
 #pragma once
 
+#define _I2C_NUMBER(num) I2C_NUM_##num
+#define I2C_NUMBER(num) _I2C_NUMBER(num)
+#define I2C_MASTER_NUM I2C_NUMBER(CONFIG_I2C_MASTER_PORT_NUM) /*!< I2C port number for master dev */
+
 void setup_i2c();
--- sensor_node/main/imu.c
+++ sensor_node/main/imu.c
@@ -1,2 +1,27 @@
+#include "esp_log.h"
+#include "esp_err.h"
+#include "driver/i2c.h"
+#include "i2c.h"
+
+static const char* TAG = "imu";
+
+static void read_chip_id() {
+	uint8_t data = 0;
+	esp_err_t err;
+	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+	err = i2c_master_start(cmd);
+	err = i2c_master_write_byte(cmd, CONFIG_IMU_ADDRESS, true);
+	err = i2c_master_read_byte(cmd, &data, false);
+	err = i2c_master_stop(cmd);
+	err = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 100);
+	if (err != ESP_OK) {
+		ESP_LOGE(TAG, "Error reading chip id for IMU: %s", esp_err_to_name(err));
+	}
+
+	i2c_cmd_link_delete(cmd);
+
+}
+
 void configure_imu() {
+	read_chip_id();
 }
