f83f98c2bd569322c134f704064d8412f96db4bd
Russell Hay <me@russellhay.com>
2019-08-26 08:22:39 -0700

    checkpoint

--- sensor_node/main/i2c.c
+++ sensor_node/main/i2c.c
@@ -1,11 +1,11 @@
 #include "driver/i2c.h"
+#include "i2c.h"
 #include "sdkconfig.h"
 
-#define _I2C_NUMBER(num) I2C_NUM_##num
-#define I2C_NUMBER(num) _I2C_NUMBER(num)
-#define I2C_MASTER_SCL_IO CONFIG_I2C_MASTER_SCL               /*!< gpio number for I2C master clock */
-#define I2C_MASTER_SDA_IO CONFIG_I2C_MASTER_SDA               /*!< gpio number for I2C master data  */
-#define I2C_MASTER_NUM I2C_NUMBER(CONFIG_I2C_MASTER_PORT_NUM) /*!< I2C port number for master dev */
+#define _GPIO(x) GPIO_NUM_##x
+#define GPIO(x) _GPIO(x)
+#define I2C_MASTER_SCL_IO GPIO(CONFIG_I2C_MASTER_SCL)               /*!< gpio number for I2C master clock */
+#define I2C_MASTER_SDA_IO GPIO(CONFIG_I2C_MASTER_SDA)               /*!< gpio number for I2C master data  */
 #define I2C_MASTER_FREQ_HZ CONFIG_I2C_MASTER_FREQUENCY        /*!< I2C master clock frequency */
 #define I2C_MASTER_TX_BUF_DISABLE 0                           /*!< I2C master doesn't need buffer */
 #define I2C_MASTER_RX_BUF_DISABLE 0                           /*!< I2C master doesn't need buffer */
@@ -25,4 +25,3 @@ static esp_err_t master_init(int port) {
 void setup_i2c() {
   master_init(I2C_MASTER_NUM);
 }
-
