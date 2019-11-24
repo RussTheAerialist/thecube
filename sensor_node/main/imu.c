3cb9680f330deee0f17a5dce3e32db0ec864a38e
Russell Hay <me@russellhay.com>
2019-11-23 16:16:59 -0800

    checkpoint

--- sensor_node/main/imu.c
+++ sensor_node/main/imu.c
@@ -7,25 +7,31 @@ static const char* TAG = "imu";
 
 static void read_chip_id() {
 	uint8_t data = 0;
-	esp_err_t err;
-	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-	ESP_ERROR_CHECK(i2c_master_start(cmd));
-	ESP_ERROR_CHECK(i2c_master_write_byte(cmd, (CONFIG_IMU_ADDRESS << 1) | I2C_MASTER_WRITE, true));
-	ESP_ERROR_CHECK(i2c_master_write_byte(cmd, 0x00, true));
-	ESP_ERROR_CHECK(i2c_master_stop(cmd));
+	{
+		i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+		ESP_ERROR_CHECK(i2c_master_start(cmd));
+		ESP_ERROR_CHECK(i2c_master_write_byte(cmd, (CONFIG_IMU_ADDRESS << 1) | I2C_MASTER_WRITE, I2C_MASTER_ACK));
+		ESP_ERROR_CHECK(i2c_master_write_byte(cmd, 0x00, I2C_MASTER_ACK));
+		ESP_ERROR_CHECK(i2c_master_start(cmd));
+		ESP_ERROR_CHECK(i2c_master_write_byte(cmd, (CONFIG_IMU_ADDRESS << 1) | I2C_MASTER_READ, I2C_MASTER_ACK));
+		ESP_ERROR_CHECK(i2c_master_read_byte(cmd, &data, I2C_MASTER_LAST_NACK));
 
-	ESP_ERROR_CHECK(i2c_master_start(cmd));
-	ESP_ERROR_CHECK(i2c_master_write_byte(cmd, (CONFIG_IMU_ADDRESS << 1) | I2C_MASTER_READ, true));
-	ESP_ERROR_CHECK(i2c_master_read_byte(cmd, &data, false));
-	ESP_ERROR_CHECK(i2c_master_stop(cmd));
-
-	err = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 100);
-	if (err != ESP_OK) {
-		ESP_LOGE(TAG, "Error reading chip id for IMU: %s", esp_err_to_name(err));
+		ESP_ERROR_CHECK(i2c_master_stop(cmd));
+		ESP_ERROR_CHECK(i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 100));
+		i2c_cmd_link_delete(cmd);
 	}
 
-	i2c_cmd_link_delete(cmd);
+	// {
+	// 	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+	// 	ESP_ERROR_CHECK(i2c_master_start(cmd));
+	// 	ESP_ERROR_CHECK(i2c_master_write_byte(cmd, (CONFIG_IMU_ADDRESS << 1) | I2C_MASTER_READ, I2C_MASTER_ACK));
+	// 	ESP_ERROR_CHECK(i2c_master_read_byte(cmd, &data, I2C_MASTER_LAST_NACK));
+	// 	ESP_ERROR_CHECK(i2c_master_stop(cmd));
+	// 	ESP_ERROR_CHECK(i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 100));
+	// 	i2c_cmd_link_delete(cmd);
+	// }
 
+	ESP_LOGI(TAG, "chip id = %02x", data);
 }
 
 void configure_imu() {
