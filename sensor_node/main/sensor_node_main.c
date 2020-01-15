ecad3f377873cdec5edb5546c0f157763dcf5d8e
Russell Hay <russell.hay@gmail.com>
2020-01-15 07:22:31 -0800

    Adding notes

--- sensor_node/main/sensor_node_main.c
+++ sensor_node/main/sensor_node_main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+
 #include "nvs_flash.h"
+#include "esp_event.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 #include "esp_app_format.h"
@@ -17,9 +19,16 @@
 
 static const char* TAG = "sensor";
 
+void event_loop(void* handler_arg, esp_event_base_t base, int32_t id, void* event_data)
+{
+  // Do Things
+}
+
 void app_main(void)
 {
   nvs_flash_init();
+
+  // Default Event Loop is created in the mesh code
 
   setup_i2c();
   configure_imu();
@@ -27,8 +36,10 @@ void app_main(void)
   configure_display();
 #endif // CONFIG_ENABLE_DISPLAY
 
-  // configure_mesh();
-  // mesh_event_loop();
+  configure_mesh();
+  mesh_event_loop(); // replace this with above event loop
+
+  // TODO: Register timer for pulling data from i2c
 
     // // TODO: Replace with event loop
     // for (int i = 10; i >= 0; i--) {
