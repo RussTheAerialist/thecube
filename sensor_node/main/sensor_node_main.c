456a9b45715972a9c2fa462db7c8ddae496c51c0
Russell Hay <russell.hay@gmail.com>
2020-01-15 20:56:02 -0800

    Output firmware version

--- sensor_node/main/sensor_node_main.c
+++ sensor_node/main/sensor_node_main.c
@@ -8,6 +8,7 @@
 #include "esp_spi_flash.h"
 #include "esp_app_format.h"
 #include "esp_ota_ops.h"
+#include "esp_partition.h"
 #include "esp_log.h"
 
 #ifdef CONFIG_ENABLE_DISPLAY
@@ -19,14 +20,19 @@
 
 static const char* TAG = "sensor";
 
-void event_loop(void* handler_arg, esp_event_base_t base, int32_t id, void* event_data)
-{
-  // Do Things
+void display_boot_header() {
+  const esp_partition_t *running = esp_ota_get_running_partition();
+
+  esp_app_desc_t running_app_info;
+  if (esp_ota_get_partition_description(running, &running_app_info) == ESP_OK) {
+      ESP_LOGI(TAG, "Running firmware version: %s", running_app_info.version);
+  }
 }
 
 void app_main(void)
 {
   nvs_flash_init();
+  display_boot_header();
 
   // Default Event Loop is created in the mesh code
 
@@ -36,8 +42,8 @@ void app_main(void)
   configure_display();
 #endif // CONFIG_ENABLE_DISPLAY
 
-  configure_mesh();
-  mesh_event_loop(); // replace this with above event loop
+  // configure_mesh();
+  // mesh_event_loop(); // replace this with above event loop
 
   start_imu_tasks();
 }
